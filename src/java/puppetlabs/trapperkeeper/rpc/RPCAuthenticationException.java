b76a7cb98b14dd372f63c4316383755b61ad1c7c
Nathaniel Smith <nathanielksmith@gmail.com>
2014-12-16 18:05:39 -0800

    Add RPCAuthenticationException.java

---
+++ src/java/puppetlabs/trapperkeeper/rpc/RPCAuthenticationException.java
@@ -0,0 +1,7 @@
+package puppetlabs.trapperkeeper.rpc;
+
+public class RPCAuthenticationException extends Exception {
+    public RPCAuthenticationException(String msg) {
+        super(msg);
+    }
+}
