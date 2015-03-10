a3c0754f7cc970543016574bbd51c21b055dd021
Nathaniel Smith <nathanielksmith@gmail.com>
2015-03-09 18:08:19 -0700

    Bump version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def ks-version "1.0.0")
 (def tk-version "1.0.1")
 
-(defproject puppetlabs/trapperkeeper-rpc "0.1.2-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-rpc "1.0.0-SNAPSHOT"
   :description "RPC server/client library for Trapperkeeper services"
   :url "https://github.com/puppetlabs/trapperkeeper-rpc"
   :pedantic? :abort
